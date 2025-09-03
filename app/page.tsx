a96997c835181386588d22ae7fc9b4e5a0d76f1a
arcana302-art <arcana302@gmail.com>
2025-09-03 16:28:51 -0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+// app/page.tsx
+import Hero from "./components/Hero";
+
+export default function Page() {
+  return (
+    <main>
+      <Hero />
+      {/* Aquí puedes seguir con “Cómo funciona”, grid de especialistas, etc. */}
+    </main>
+  );
+}
