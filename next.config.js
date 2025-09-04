b1d7d9a781d1386e275bf03078eb4eec0946f1cd
arcana302-art <arcana302@gmail.com>
2025-09-04 02:25:02 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true }, // <-- permite compilar aunque haya errores de TS
-  eslint: { ignoreDuringBuilds: true },    // <-- no corta el build por ESLint
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true }
 };
 
 module.exports = nextConfig;
